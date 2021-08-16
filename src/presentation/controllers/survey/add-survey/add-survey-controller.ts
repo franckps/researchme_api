d4b278fc455c9d12182436693b4267458de06ba2
Francisco Pereira <fran.p.s@hotmail.com>
2021-08-16 09:18:05 -0300

    fix: change AddSurveyController to use a proise for return a badRequest error on validation fail

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -8,7 +8,7 @@ export class AddSurveyController implements Controller {
     handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const error = this.validation.validate(httpRequest.body)
         if(error){
-            return badRequest(error)
+            return new Promise(resolve => resolve(badRequest(error)))
         }
         return new Promise(resolve => resolve(null))
     }
