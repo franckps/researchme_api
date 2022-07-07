0363097dd45b80d499d38dc589e4ec73d96935e9
Francisco Pereira <fran.p.s@hotmail.com>
2022-07-07 09:08:21 -0300

    fix: change the routes configuration to excludes '.map'

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,11 @@
 import { Express, Router } from 'express'
-import fg from 'fast-glob'
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => (
-    (await import(`../../../${file}`)).default(router)
-  ))
+  readdirSync(resolve(__dirname,'..','routes')).map(async file => {
+    if (!file.includes('.test.') && !file.endsWith('.map')) { (await import(`../routes/${file}`)).default(router) }
+  })
 }
