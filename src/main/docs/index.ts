d5d4297c947e3999f29fb8ce8e868b312ece0aae
Francisco Pereira <fran.p.s@hotmail.com>
2022-07-28 13:59:36 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './login-path';
+import { accountSchema } from './schemas/account-schema';
+import { loginParamsSchema } from './schemas/login-params-schema';
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'research_me',
     description: "An simple survey's api",
     version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+  tags: [{ name: 'Login' }],
+  paths: {
+    '/login': loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
   },
 };
