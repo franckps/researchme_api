acad0a1748af0c5922df7ab006ed904559f02a62
Francisco Pereira <fran.p.s@hotmail.com>
2022-07-29 23:54:05 -0300

    refactor: change save survey result return

--- src/domain/models/survey-result.ts
+++ src/domain/models/survey-result.ts
@@ -1,7 +1,20 @@
-export type SurveyResultModel = {
-  id: string;
+export type SurveyResultParams = {
   surveyId: string;
   accountId: string;
   answer: string;
   date: Date;
+};
+
+export type SurveyResultModel = {
+  surveyId: string;
+  question: string;
+  answers: SurveyAnswerModel[];
+  date: Date;
+};
+
+type SurveyAnswerModel = {
+  image?: string;
+  answer: string;
+  count: number;
+  percent: number;
 };
--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -1,13 +1,29 @@
-import { SurveyResultModel } from '@/domain/models/survey-result';
+import {
+  SurveyResultModel,
+  SurveyResultParams,
+} from '@/domain/models/survey-result';
 import { AddSurveyParams } from '../usecases/survey/add-survey';
 
-export const mockSurveyResultModel = (): SurveyResultModel =>
-  Object.assign({}, mockSaveSurveyResultParams(), { id: 'any_id' });
+export const mockSurveyResultModel = (): SurveyResultModel => ({
+  surveyId: 'any_surveyId',
+  question: 'any_question',
+  answers: [
+    {
+      answer: 'any_answer',
+      count: 1,
+      percent: 50,
+    },
+    {
+      answer: 'other_answer',
+      image: 'any_image',
+      count: 10,
+      percent: 80,
+    },
+  ],
+  date: new Date(),
+});
 
-export const mockSaveSurveyResultParams = (): Omit<
-  SurveyResultModel,
-  'id'
-> => ({
+export const mockSaveSurveyResultParams = (): SurveyResultParams => ({
   accountId: 'any_account_id',
   surveyId: 'any_account_id',
   answer: 'any_answer',
