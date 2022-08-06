3b250b3042691debd72042d3815b5c9bc90abdeb
Francisco Pereira <fran.p.s@hotmail.com>
2022-08-06 11:55:31 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct value

---
+++ src/data/usecases/survey-result/load-survey-result/db-save-survey-result.ts
@@ -0,0 +1,16 @@
+import {
+  LoadSurveyResult,
+  SurveyResultModel,
+  LoadSurveyResultRepository,
+} from './db-load-survey-result-protocols';
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor(
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+  ) {}
+
+  async load(surveyId: string): Promise<SurveyResultModel> {
+    await this.loadSurveyResultRepository.loadBySurveyId(surveyId);
+    return Promise.resolve(null);
+  }
+}
