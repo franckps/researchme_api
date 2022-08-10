17beecd07116c6953565f8c51891d9b37e2afa8c
Francisco Pereira <fran.p.s@hotmail.com>
2022-08-10 08:51:56 -0300

    feat: ensure load survey result route returns 403 if no accesToken is provided

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -2,11 +2,17 @@ import { Router } from 'express';
 import { auth } from '../middlewares/auth';
 import { adaptRoute } from '../adapters/express/express-route-adapter';
 import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory';
+import { makeLoadSurveyResultController } from '../factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory';
 
 export default (router: Router): void => {
   router.put(
     '/surveys/:surveyId/results',
     auth,
     adaptRoute(makeSaveSurveyResultController())
+  );
+  router.get(
+    '/surveys/:surveyId/results',
+    auth,
+    adaptRoute(makeLoadSurveyResultController())
   );
 };
